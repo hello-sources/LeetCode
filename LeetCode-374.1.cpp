918bf6f5daf3db00d87a5c642a848479ffdcec37
ltw <3245849061@qq.com>
2020-05-17 15:27:18 +0800

    进制骚操作

--- LeetCode-374.1.cpp
+++ LeetCode-374.1.cpp
@@ -15,7 +15,8 @@ public:
         int mid, ret;
         while (l <= r) {
             mid = ((l^r) >> 1) + (l & r);//二进制骚操作
-            if ((ret = guess(mid)) == 0) return mid;
+			//表示中间数是由他们俩相同的数加上不同数的平均值得来的，同时也适用于long long类型
+			if ((ret = guess(mid)) == 0) return mid;
             if (ret == -1) r = mid - 1;
             else l = mid + 1;
         }
