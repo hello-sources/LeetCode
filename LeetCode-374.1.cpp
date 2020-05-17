51b562457cd1499e0a7f0a212b46b9d0ab752958
ltw <3245849061@qq.com>
2020-05-17 15:13:24 +0800

    力扣374二进制骚操作

---
+++ LeetCode-374.1.cpp
@@ -0,0 +1,27 @@
+/*************************************************************************
+    > File Name: LeetCode-374.1.cpp
+    > Author: ltw
+    > Mail: 3245849061@qq.com 
+    > Created Time: Sun 17 May 2020 03:12:21 PM CST
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+
+class Solution {
+public:
+    int bs(int l, int r) {
+        int mid, ret;
+        while (l <= r) {
+            mid = ((l^r) >> 1) + (l & r);//二进制骚操作
+            if ((ret = guess(mid)) == 0) return mid;
+            if (ret == -1) r = mid - 1;
+            else l = mid + 1;
+        }
+        return -1;
+    }
+    int guessNumber(int n) {
+        return bs(1, n);
+    }
+};
